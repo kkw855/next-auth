2041d5d4dea8985bc330375a5859ff111b8bf8fa
kkw855 <kkw855@gmail.com>
2024-08-03 15:24:53 +0900

    ➕ dependencies: @storybook/addon-a11y 설치, 설정

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -5,7 +5,7 @@ const config: StorybookConfig = {
     '../app/**/*.stories.@(js|jsx|ts|tsx)',
     '../components/**/*.stories.@(js|jsx|ts|tsx)',
   ],
-  addons: ['@storybook/addon-essentials'],
+  addons: ['@storybook/addon-essentials', '@storybook/addon-a11y'],
   framework: '@storybook/nextjs',
   features: { experimentalRSC: true },
 }
