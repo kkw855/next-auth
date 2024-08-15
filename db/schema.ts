b749a0b836c41ba505485d6c48bfe7f1a8d5ab79
kkw855 <kkw855@gmail.com>
2024-08-15 15:35:25 +0900

    ✨ feat: Auth.js 이메일, 패스워드를 위한 스키마 추가

--- db/schema.ts
+++ db/schema.ts
@@ -1,9 +1,44 @@
-import { pgTable, text } from 'drizzle-orm/pg-core'
+import {
+  pgTable,
+  text,
+  timestamp,
+  integer,
+  primaryKey,
+} from 'drizzle-orm/pg-core'
 import { createId } from '@paralleldrive/cuid2'
+import { AdapterAccountType } from '@auth/core/adapters'
 
 export const users = pgTable('user', {
   id: text('id')
     .primaryKey()
     .$defaultFn(() => createId()),
-  name: text('name').notNull(),
+  name: text('name'),
+  email: text('email').unique(),
+  emailVerified: timestamp('emailVerified', { mode: 'date' }),
+  image: text('image'),
+  password: text('password'),
 })
+
+export const accounts = pgTable(
+  'account',
+  {
+    userId: text('userId')
+      .notNull()
+      .references(() => users.id, { onDelete: 'cascade' }),
+    type: text('type').$type<AdapterAccountType>().notNull(),
+    provider: text('provider').notNull(),
+    providerAccountId: text('providerAccountId').notNull(),
+    refresh_token: text('refresh_token'),
+    access_token: text('access_token'),
+    expires_at: integer('expires_at'),
+    token_type: text('token_type'),
+    scope: text('scope'),
+    id_token: text('id_token'),
+    session_state: text('session_state'),
+  },
+  (account) => ({
+    compoundKey: primaryKey({
+      columns: [account.provider, account.providerAccountId],
+    }),
+  }),
+)
